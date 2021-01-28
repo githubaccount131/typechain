97e1c7953ad1c6ad69f437e8f792a8c10434d3aa
ento32940 <netlify@gmail.com>
2021-01-28 14:18:48 +0900

    Blockchain Part 2

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,28 @@
+import * as CryptoJS from "crypto-js";
+
 class Block {
   public index: number;
   public hash: string;
   public previousHash: string;
   public data: string;
   public timestamp: number;
-  
-  constructor (index: number, hash: string, previousHash: string, data: string, timestamp: number) {
+
+  static caculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string => {
+    return CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+  };
+
+  constructor (
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
     this.index = index;
     this.hash = hash;
     this.previousHash = previousHash;
@@ -16,8 +33,14 @@ class Block {
 
 const genesisBlock: Block = new Block(0, "20202020202", "", "Hello", 123456);
 
-let blockChain: [Block] = [genesisBlock];
+let blockChain: Block[] = [genesisBlock];
 
 console.log(blockChain);
+
+const getBlockchain = (): Block[] => blockChain;
+
+const getLatestBlock = (): Block => blockChain[blockChain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
 
 export {};
